887f70ad6fca314a5d4362e888dbbda33e3dd4ed
wryanj <ryanjohnson9685@gmail.com>
2021-02-14 15:19:47 -0500

    updated with some working functions for readme content

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
     const fs = require("fs"); // Included with node
     const util = require("util"); // Included with node
 
-// Utilize inquirer to get needed information for writing a Readme file with content
+
+// Define Function to utilize inquirer to get needed information for writing a Readme file with content
     const promptUser = () => {
         return inquirer.prompt ([
             {
@@ -39,13 +40,24 @@
         ])    
     }
 
+// Define function that takes in an inquirer response and generates some content for a readme...
+
+    // Arrow function syntax says function is called generate Readme, takes in inquirer response as parameter...
+    const generateReadMe = inquirerResponse => {
+
+        // And returns some readme content that I will use template literals to fill in...
+        return `
+        some readme content..
+        `
+    }
+
+// Define a function for doing something with the Response I get....
     promptUser().then(response => {
-        //Do something with the response ex...
-        console.log(response.title);
-        console.log(response.description);
+        const readMeContent = generateReadMe(response)
+        console.log(readMeContent);
     })
 
-// TODO: Create a function to write README file
+
 //function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
