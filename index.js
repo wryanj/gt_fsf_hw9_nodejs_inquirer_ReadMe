137b5c305eecf2252bb58478b27beafd190dac5e
wryanj <ryanjohnson9685@gmail.com>
2021-02-14 14:07:51 -0500

    updated testfiles directory and specified to ignore, started defining my questions array

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Utilize inquirer to get needed information for writing a Readme file with content
+function promptUser () {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your ReadME?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "What is the description of this application?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What is needed to install this application?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "How should this application be used?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who contributed to this application"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What information is availible to help with testing this application?"
+        },
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
