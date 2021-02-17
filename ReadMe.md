5b696f82019a8f325a5eaa43bc1bdfcba0f78d9b
wryanj <ryanjohnson9685@gmail.com>
2021-02-17 16:31:40 -0500

    updated code with comments for readability

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,5 @@
 
-# TITLE TEST
+# Testing
 ## Table of Contents
 1. [Description](#Description)
 2. [Installation](#Installation)
@@ -7,19 +7,19 @@
 4. [Contributing](#Contributing);
 5. [Tests](#Tests)
 ## Description
-
+T
 ## Installation
-
+R
 ## Usage
-
+Y
 ## Contribution Guidelines
-
+A
 ## Tests
-
+N
 ## Licenses
-![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)  
-https://opensource.org/licenses/MIT
+undefined  
+undefined
 ## Questions
-Email me at  for more information.
-You can also find me on github with the user name . 
+Email me at J for more information.
+You can also find me on github with the user name H. 
 Check my profile out here: https://github.com/wryanj
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-// Up front dependencies, global variale definitions & conversions
+//-------------------------------------------------------------------------------------------------------------
+// DECLARE DEPENDENCIES, GLOBAL VARIABLES AND INITIAL CONVERSIONS
+//-------------------------------------------------------------------------------------------------------------
 
     // Include packages needed for this application
     const inquirer = require("inquirer"); 
     const fs = require("fs"); 
     const util = require("util"); 
 
-    // Define global variables to set value to later
+    // Define global variables that will have values established in later functions
     let licenceBadge;
     let licenceLink;
         
-    // Use promisify to convert fs.writefile method so that it returns response in a promise object rather than using a callback function
+    // Use promisify to convert fs.writefile method so that it returns response in a promise object 
     const writeFileAsync = util.promisify(fs.writeFile);
 
-// Define Function to utilize inquirer to get needed information for writing a Readme file with the responses
+//---------------------------------------------------------------------------------------------------------------
+// FUNCTIONAL PROGRAM LOGIC TO BE EXEUCUTED UPON INVOKATION OF INIT() FUNCTION AT BOTTOM OF SCRIPT
+//---------------------------------------------------------------------------------------------------------------
+
+    // When the applicatoin starts, collect information via prompts using inquirer....
     const promptUser = () => {
     
         return inquirer.prompt ([
@@ -65,62 +71,57 @@
         ])    
     }
 
-// Define Function to render a liscence badge based on inquirer input...
+    // Determine what badge to display based on license type selected during the prompt...
     const renderLicenseBadge = (x) => {
         
         if (x === "Apache 2.0") {
             licenceBadge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
         }
-      
         else if (x === "MIT") {
             licenceBadge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
         }
-      
         else if (x === "GPL 3.0") {
             licenceBadge = "![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)";
         }
-      
         else if (x === "BSD 3") {
             licenceBadge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
         }
-      
         else if (x === "None") {
             licenceBadge = "";
         }
       }
 
-// Define function to render a licence link based on inquirer input..
+    // Determine what license link to display based on license type selected during the prompt...
     const renderLicenseLink = (x) => {
         
         if (x === "Apache 2.0") {
             licenceLink = "https://opensource.org/licenses/Apache-2.0";
         }
-    
         else if (x === "MIT") {
             licenceLink = "https://opensource.org/licenses/MIT";
         }
-    
         else if (x === "GPL 3.0") {
             licenceLink = "http://www.gnu.org/licenses/gpl-3.0";
         }
-    
         else if (x === "BSD 3") {
             licenceLink = "https://opensource.org/licenses/BSD-3-Clause";
         }
-    
         else if (x === "None") {
             licenceLink = "";
         }
     }
 
-// Define function that takes in an inquirer response and generates some content for a readme...
-// Arrow function syntax says function is called generate Readme, takes in inquirer response as parameter and is using "function paramater desstructuring" to destruction the param...
-const generateReadMeContent = ({title,description, installation, usage, contributing, tests, email, github, licences}) => { 
-// Get Liscence badge
-renderLicenseBadge(licences);
-// Get Liscence link
-renderLicenseLink(licences);
-// And return this content into a new (or existing) readme file in this directory (RESEARCH OUTDENT SO I CAN CODE CLEAR BUT NOT HAVE SPACING IN CONTENT)...
+    // Generate content to write to a ReadMe.md file using information collected in the prompt session....
+    const generateReadMeContent = ({title,description, installation, usage, contributing, tests, email, github, licences}) => { 
+
+        // Get Liscence badge and assign it to a global variable...
+        renderLicenseBadge(licences);
+
+        // Get Liscence link and assign it to a global variable...
+        renderLicenseLink(licences);
+
+        // Use the destructured response object properties and additional text to generate the content for the ReadMe...
+
 return `
 # ${title}
 ## Table of Contents
@@ -149,30 +150,38 @@ Check my profile out here: https://github.com/wryanj
 `
 }
 
-// Define a function that kicks off the question / answer / do something sequence using functions defined above....
-const init = () => {
+//--------------------------------------------------------------------------------------------------------------------------------
+//DEFINE SEQUENCE FOR ASYNCHRONOUS LOGIC EXECUTION
+//--------------------------------------------------------------------------------------------------------------------------------
 
-    // Upon Call of init, Run the prompt user function...
-    promptUser()
+    // Wrap sequence in a single function to be declaritive...
+    const init = () => {
 
-        // Then, when the function is completed....(whats it doing with response...)
-        .then(response => {
+        // Upon Call of init, prompt the user for inputs
+        promptUser()
 
-            // Define a variable that holds the content I want to write to a readme file, which is generated via invoking the variable holding the function I defined to do this...
-            const readMeContent = generateReadMeContent(response)
-           
-            // Then call the promisified fs.writeFile method to create (or write content to) the file ReadMe.md (in this directory), including the readMeContent I defined..
-            writeFileAsync("ReadMe.md", readMeContent) 
+            // Then, when the function is completed take the result (the prompt response)...
+            .then(response => {
 
-        // Then, when the writeFileAsync is completed, if no error console log "success"
-        .then(() => console.log("Success"))
+                // And pass it as a parameter to the generate readme content so I can generate content to write to a file...
+                const readMeContent = generateReadMeContent(response)
+            
+                // Once conetent is generated, write the file using promisified fs.writeFile method....
+                writeFileAsync("ReadMe.md", readMeContent) 
 
-        // If after writeFileAsyc completes, an error is detected.. console log the error
-        .catch(err => console.error(err));
-    })
-}
+            // Then, when the writeFileAsync is completed, if no error console log "success"...
+            .then(() => console.log("Success"))
+
+            // If at any time in the chain there is a failure, console log an error and stop the sequence..
+            .catch(err => console.error(err));
+        })
+    }
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+//START PROGRAM
+//-----------------------------------------------------------------------------------------------------------------------------------
  
-// Call Init Function to start the chain of asking questions, getting answers, then writing a new file (everything I declared above)...
-init();
+    // Invoke the init functoin to run the program...
+    init();
 
 
